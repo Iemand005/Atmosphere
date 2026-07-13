2f46c58b3c52698b7b43109a89e458345bf2b5b9
Lasse Lauwerys <lauweryslasse@gmail.com>
2026-07-13 20:53:39 +0200

    no lite

--- resources/shaders/FragmentShader_vk.glsl
+++ resources/shaders/FragmentShader_vk.glsl
@@ -6,39 +6,7 @@ layout (location = 0) in vec3 Normal;
 layout (location = 1) in vec3 FragPos;
 layout (location = 2) in vec2 TexCoord;
 
-struct PointLight {
-    vec3 position;
-    vec3 color;
-    float intensity;
-    float radius;
-};
-
-layout (binding = 0) uniform sampler2D ourTexture;
-
-layout (binding = 1) uniform LightingUBO {
-    int lightCount;
-    PointLight pointLights[8];
-};
-
 void main()
 {
-    vec3 n = normalize(Normal);
-
-    vec3 lighting = vec3(0.1);
-    for (int i = 0; i < lightCount; ++i) {
-        vec3 L = pointLights[i].position - FragPos;
-        float dist = length(L);
-        if (dist < 0.0001) continue;
-        vec3 ldir = L / dist;
-
-        float diff = max(dot(n, ldir), 0.0);
-        float radius = max(pointLights[i].radius, 0.001);
-        float atten = 1.0 / (1.0 + (dist * dist) / (radius * radius));
-
-        vec3 contrib = pointLights[i].color * pointLights[i].intensity * diff * atten;
-        lighting += contrib;
-    }
-
-    vec4 texSample = texture(ourTexture, TexCoord);
-    FragColor = vec4(texSample.rgb * lighting, texSample.a);
+    FragColor = vec4(0.8, 0.4, 0.2, 1.0);
 }
